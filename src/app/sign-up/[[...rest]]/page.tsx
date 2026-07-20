74e913643f35fa2bb4d86cfd2ecf9de961939a1a
GuoHeTech <917579155@qq.com>
2026-07-20 18:53:00 +0800

    fix: add afterSignInUrl/afterSignUpUrl to SignIn/SignUp components

--- src/app/sign-up/[[...rest]]/page.tsx
+++ src/app/sign-up/[[...rest]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs"
 export default function Page() {
   return (
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100vh", background: "#0a0a0f" }}>
-      <SignUp />
+      <SignUp afterSignUpUrl="/sign-in?registered=true" />
     </div>
   )
 }
